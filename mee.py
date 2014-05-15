1b57a074e819149d280847466d52ed6d67165ffb
Michael Stehmann <michael@lando>
2014-05-15 22:30:15 +0200

    V011

--- mee.py
+++ mee.py
@@ -1,21 +1,18 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-
 cr="""  mee.py
-Michaels einfacher Editor - Version 0.1.0
+Michaels einfacher Editor - Version 0.1.1
   
 Copyright 2014 Michael Stehmann <info@rechtsanwalt-stehmann.de>
   
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation; either version 3 of the License, or
-(at your option) any later version.
-  
+(at your option) any later version.\n  
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-  
+GNU General Public License for more details.\n
 You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
@@ -27,7 +24,7 @@ import os, tkinter.filedialog, tkinter.scrolledtext, tkinter.messagebox
 class Editor(object):
 	
 	def __init__(self):
-		self.content = ""; self.e = False; self.n = False
+		self.content, self.endflag, self.newflag = "", False, False
 		if not hasattr(self, 'fs'): self.fs=10
 		self.master = Tk()
 		self.master.title("Editor")
@@ -52,48 +49,50 @@ class Editor(object):
 		viewmenu.add_command(label="Schrift vergrößern", command=self.enlargefont, font = "SansSerif, "+str(self.fs))
 		viewmenu.add_command(label="Schrift verkleinern", command=self.reducefont, font = "SansSerif, "+str(self.fs))
 		menu.add_command(label="Informationen", command=self.minfo, font = "SansSerif, "+str(self.fs))
-		
+
 	def makenew(self):
-		self.n == True
+		self.newflag = True
 		self.ende()
 		self.__init__()
-		
+
 	def enlargefont(self):
 		self.fs = self.fs+1
+		self.st["font"] = "SansSerif, "+str(self.fs)
 		self.menu()
-		
+
 	def reducefont(self):
 		if self.fs > 6:
 			self.fs = self.fs-1
+			self.st["font"] = "SansSerif, "+str(self.fs)
 			self.menu()
 		else: tkinter.messagebox.showwarning("Kleiner geht nicht!", "Noch kleiner ist nicht vorgesehen.", type="ok")
-		
+
 	def ende(self):
 		if self.st.get("0.0", "end") != "\n" and self.st.get("0.0", "end") != self.content:
 			if tkinter.messagebox.askyesno("Speichern?", "Vorher speichern?",icon="question"):
-				if not self.n: self.e = True
+				if not self.newflag:
+					self.endflag = True
 				self.saver()
-			else: self.master.destroy()			 
+			else: self.master.destroy()
 		else: self.master.destroy()
-		
+
 	def minfo(self):
 		sb = tkinter.messagebox.showinfo("Informationen", cr, type="ok")
-			
+
 	def opener(self):
 		try:
 			fo = tkinter.filedialog.askopenfile(initialdir=os.getenv('HOME'))
 			self.st.insert(INSERT,fo.read())
-		except: return
-	
+		except: return # Faengt Abbruch des Dateiladendialoges ab
+
 	def saver(self):
 		try:
 			nfo = tkinter.filedialog.asksaveasfile(initialdir=os.getenv('HOME'))
 			nfo.write(self.st.get("0.0", "end"))
 			self.content = self.st.get("0.0", "end")
-			if self.e == True: self.ende()
-			if self.n == True: self.newdia()
-		except: return
-		
+			if self.endflag == True: self.ende()
+			if self.newflag == True: self.newdia()
+		except: tkinter.messagebox.showwarning("Achtung!", "Es wurde nichts gespeichert", type="ok")
+
 if __name__ == '__main__':
 	mee = Editor()
-
