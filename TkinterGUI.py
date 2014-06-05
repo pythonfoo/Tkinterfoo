03d47a7f91fddb0874f16fef416eefe26edb225e
Mechtilde <ooo@mechtilde.de>
2014-06-05 20:32:41 +0200

    TkinterGUI2

--- TkinterGUI.py
+++ TkinterGUI.py
@@ -30,16 +30,25 @@ def _(String):
 	"""solange keine gettext Integration erfolgt ist, ist dies ein dummy"""
 	return String
 
-class createWindow(tkinter.Frame):
+class createWindow(Frame):
 
 	def __init__(self, master):
 		self.master = master
-		self.menuBar = tkinter.Menu(master)
+		self.menuBar = Menu(master)
 		self.master.config(menu = self.menuBar)
 		self.fillMenuBar()
+		self.createwindow()
+
+	def createwindow(self):
+		# Erzeugung eines Fensters
+		tkFenster = Tk()
+		tkFenster.title("Kalender")
+		# Erzeugung einer Variablen Größe
+		tkFenster.geometry("180x145")
+
 
 	def fillMenuBar(self):
-		self.menuFile=tkinter.Menu(self.menuBar, tearoff=False)
+		self.menuFile=Menu(self.menuBar, tearoff=False)
 		self.menuBar.add_cascade(label = "Kalender", menu=self.menuFile)
 		self.menuFile.add_command(label = "Fenster", command = self.handler)
 		self.menuFile.add_command(label = "Einfach", command = self.einfacheAnzeige)
@@ -47,7 +56,7 @@ class createWindow(tkinter.Frame):
 		self.menuFile.add_separator()
 		self.menuFile.add_command(label = "Beenden", command = self.quit)
 
-		self.menuFile1=tkinter.Menu(self.menuBar, tearoff=False)
+		self.menuFile1=Menu(self.menuBar, tearoff=False)
 		self.menuBar.add_cascade(label = "Datei", menu=self.menuFile1)
 		self.menuFile1.add_command(label = "Neu", command = self.handler)
 		self.menuFile1.add_command(label = "Öffnen", command = self.handler)
@@ -56,7 +65,7 @@ class createWindow(tkinter.Frame):
 		self.menuFile1.add_separator()
 		self.menuFile1.add_command(label = "Beenden", command = self.quit)
 
-		self.menuFile2=tkinter.Menu(self.menuBar, tearoff=False)
+		self.menuFile2=Menu(self.menuBar, tearoff=False)
 		self.menuBar.add_cascade(label = "Bearbeiten", menu=self.menuFile2)
 		self.menuFile2.add_command(label = "Suchen", command = self.handler)
 		self.menuFile2.add_command(label = "Ersetzen", command = self.handler)
@@ -65,9 +74,9 @@ class createWindow(tkinter.Frame):
 		self.menuFile2.add_command(label = "Kopieren", command = self.handler)
 		self.menuFile2.add_command(label = "Einfügen", command = self.handler)
 
-	def einfacheAnzeige(self):
+	def einfacheAnzeige(self,):
 		# Label für die Anzeige der Daten
-		labelMonat = Label(master=tkFenster, text='Januar', fg='white', bg='gray', font=('Arial', 16))
+		labelMonat = Label(master=self.tkFenster, text='Januar', fg='white', bg='gray', font=('Arial', 16))
 		labelMonat.place(x=5, y=5, width=170, height=20)
 		labelTag = Label(master=tkFenster, text='21', fg='red', bg='#FFCFC9', font=('Arial', 72))
 		labelTag.place(x=5, y=30, width=170, height=80)
@@ -83,7 +92,7 @@ class createWindow(tkinter.Frame):
 
 def main():
 	# Erzeugung eines Fensters
-	tkFenster =Tk()
+	tkFenster = Tk()
 	tkFenster.title("Kalender")
 	# Erzeugung einer Variablen Größe
 	tkFenster.geometry("180x145")
